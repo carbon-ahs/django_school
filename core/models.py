0eebf7888dfd840f1cfbc4090f1d325954d5f74d
carbon-ahs <shehanuk.ahsan@gmail.com>
2023-12-12 16:14:12 +0600

    teachers model done

--- core/models.py
+++ core/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Teacher(models.Model):
+    """model class"""
+
+    name = models.CharField(_("Name"), max_length=254)
+    designation = models.CharField(_("Designation"), max_length=50)
+    fb_link = models.CharField(
+        _("Facebook"),
+        max_length=255,
+        null=True,
+        blank=True,
+        default="#",
+    )
+    linkedIn_link = models.CharField(
+        _("LinkedIn"),
+        max_length=255,
+        null=True,
+        blank=True,
+        default="#",
+    )
+    phone_number = models.CharField(_("Phone"), max_length=255, null=True, blank=True)
+    picture_url = models.ImageField(
+        upload_to="upload_image/",
+    )
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse("Teacher_detail", kwargs={"pk": self.pk})
