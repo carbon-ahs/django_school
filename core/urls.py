fd16badd8d3c686404cfa7098ee060b358a16cd1
carbon-ahs <shehanuk.ahsan@gmail.com>
2023-12-17 17:58:56 +0600

    Notice Page added

--- core/urls.py
+++ core/urls.py
@@ -8,10 +8,11 @@ from . import views
 # from .views import say_hello
 
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("", views.home, name="home"),
     path("teachers/", views.teachers, name="teachers"),
     path("teacher/<int:pk>/", views.single_teacher, name="single_teacher"),
     path("test/", views.say_hello, name="hlw"),
     path("test2/", views.test, name="test"),
     path("about/", views.test, name="about"),
+    path("notice/", views.notice, name="notice"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -2,21 +2,7 @@ from django.shortcuts import render
 from core.models import Teacher
 
 
-def say_hello(request):
-    context = {
-        "test": "TEST",
-    }
-    return render(request, "test/hlw.html", context=context)
-
-
-def test(request):
-    context = {
-        "test": "TEST",
-    }
-    return render(request, "test.html", context=context)
-
-
-def index(request):
+def home(request):
     description_title = "Viqarunnisa Noon School & College"
     description = "Viqarunnisa Noon School & College is an all-girls educational institute in Baily Road, Dhaka, Bangladesh. It has 4 campuses and around 25,000 students. Viqarunnisa Noon School & College is one of the renowned educational institutes in Bangladesh. We consider every child as unique and so we maintain inclusive learning-teaching environment at every step in our great set-up. It is a fact now that our results are getting better in the public examinations every time. It has been made possible through our extensive and effective care stretched out to every individual student. Our students conglomerate here from multifarious backgrounds; various strata of the society. They enter the threshold of our strong and fortified home of learning and come out bearing an all-round personality."
 
@@ -53,3 +39,24 @@ def single_teacher(request, pk):
         "teacher": teacher,
     }
     return render(request, "core/single_teacher.html", context=context)
+
+
+def notice(request):
+    context = {
+        "test": "TESTnotice",
+    }
+    return render(request, "core/notice.html", context=context)
+
+
+def say_hello(request):
+    context = {
+        "test": "TEST",
+    }
+    return render(request, "test/hlw.html", context=context)
+
+
+def test(request):
+    context = {
+        "test": "TEST",
+    }
+    return render(request, "test/test.html", context=context)
